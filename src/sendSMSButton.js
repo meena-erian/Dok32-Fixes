234a79f86cd88c1a07e9d32f16a9184af36ed2fc
Menas Erian <meena5.erian@gmail.com>
2020-10-05 15:21:18 +0400

    Add the modal trigger function

--- src/sendSMSButton.js
+++ src/sendSMSButton.js
@@ -1,13 +1,13 @@
 import {sendSMSModal} from "./sendSMSModal.js";
 
 function sendSMSButton(){
-
+    document.body.append(sendSMSModal());
     console.log("Dok32 Fixes extension: Adding new report function button \"Send SMS\"");
     let exportButton = document.createElement("button");
     exportButton.className = "btn btn-success btn-sm";
     exportButton.id = "download-CSV-button";
     exportButton.onclick = (function () {
-        document.body.append(sendSMSModal());
+        $("#smsModal").modal();
     });
     exportButton.innerText = "Send SMS";
     return exportButton;
