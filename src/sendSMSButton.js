e88b9c131b131e0cdbda0e9528049239e409a89e
Menas Erian <meena5.erian@gmail.com>
2020-10-05 17:08:05 +0400

    Update sendSMSButton.js

--- src/sendSMSButton.js
+++ src/sendSMSButton.js
@@ -6,7 +6,7 @@ function sendSMSButton(){
     console.log("Dok32 Fixes extension: Adding new report function button \"Send SMS\"");
     let exportButton = document.createElement("button");
     exportButton.className = "btn btn-success btn-sm";
-    exportButton.id = "download-CSV-button";
+    exportButton.id = "send-SMS-button";
     exportButton.onclick = (function (){
         //$("#smsModal").modal();
         console.log(window);
