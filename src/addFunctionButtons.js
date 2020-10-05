e223553e3a786258531a455d460d86e8907fc950
Menas Erian <meena5.erian@gmail.com>
2020-10-05 14:15:44 +0400

    Add spaces

--- src/addFunctionButtons.js
+++ src/addFunctionButtons.js
@@ -5,8 +5,11 @@ function addFunctionButtons(){
 	let greenButtons = document.querySelectorAll("button.btn-success");
 	greenButtons.forEach((btn) => {
 		if (btn.innerText.includes("XLS")) {
-            btn.parentElement.appendChild(downloadCSVButton());
-            btn.parentElement.appendChild(sendSMSButton());
+            btn.parentElement.append("  ");
+            btn.parentElement.append(downloadCSVButton());
+            btn.parentElement.append("  ");
+            btn.parentElement.append(sendSMSButton());
+            btn.parentElement.append("  ");
         }
     });
 }
