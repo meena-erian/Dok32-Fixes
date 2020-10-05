dc5ddb16183c33efbd11e3c030147185109511f5
Menas Erian <meena5.erian@gmail.com>
2020-10-05 15:12:55 +0400

    inesrt modal

--- src/sendSMSButton.js
+++ src/sendSMSButton.js
@@ -1,10 +1,13 @@
+import {sendSMSModal} from "./sendSMSModal.js";
+
 function sendSMSButton(){
+
     console.log("Dok32 Fixes extension: Adding new report function button \"Send SMS\"");
     let exportButton = document.createElement("button");
     exportButton.className = "btn btn-success btn-sm";
     exportButton.id = "download-CSV-button";
     exportButton.onclick = (function () {
-        
+        document.body.append(sendSMSModal());
     });
     exportButton.innerText = "Send SMS";
     return exportButton;
