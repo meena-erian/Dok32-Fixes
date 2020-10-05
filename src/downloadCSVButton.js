eb6a35ad4e9a59ecfc9d9914130c5633d271242b
Menas Erian <meena5.erian@gmail.com>
2020-10-05 14:06:35 +0400

    rename module export

--- src/downloadCSVButton.js
+++ src/downloadCSVButton.js
@@ -1,4 +1,4 @@
-function addDownloadCSV(){
+function downloadCSVButton(){
 	console.log("Dok32 Fixes extension: Adding new report function button \"Download CSV\"");
 	let exportButton = document.createElement("button");
 	exportButton.className = "btn btn-success btn-sm";
@@ -27,4 +27,4 @@ function addDownloadCSV(){
 	exportButton.innerText = "Download CSV";
 	return exportButton;
 }
-export {addDownloadCSV};
+export {downloadCSVButton};
