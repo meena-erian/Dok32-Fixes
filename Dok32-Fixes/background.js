72d033afbb2d1dc2056ca61c9037f00bd9ecd5a2
Menas Erian <meena5.erian@gmail.com>
2020-10-05 12:17:58 +0400

    Add client-side extension code sync

---
+++ Dok32-Fixes/background.js
@@ -0,0 +1,6 @@
+chrome.webNavigation.onDOMContentLoaded.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'inject.js'
+	});
+});
---
+++ Dok32-Fixes/inject.js
@@ -0,0 +1,5 @@
+(function () {
+	import("https://raw.githubusercontent.com/meena-erian/Dok32-Fixes/master/src/index.js").then(indexModule =>
+		indexModule.default()
+	);
+})();
