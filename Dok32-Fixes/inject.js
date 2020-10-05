b511dc2e06afe7acd1bf2e7d321b0915fecf859c
Menas Erian <meena5.erian@gmail.com>
2020-10-05 13:31:07 +0400

    Configuring the extension to pre-load all its modules for better performance

--- Dok32-Fixes/inject.js
+++ Dok32-Fixes/inject.js
@@ -1,4 +1,8 @@
 (function () {
+	let mpreload = document.createElement("LINK");
+	mpreload.rel = "modulepreload";
+	mpreload.href = "https://everlast.portacode.com/Dok32-fixes/index.js";
+	document.head.append(mpreload);
 	import("https://everlast.portacode.com/Dok32-fixes/index.js").then(indexModule =>
 		indexModule.default()
 	);
