3be90840f78ff95f01310de35224c747404123a8
Menas Erian <meena5.erian@gmail.com>
2020-10-05 13:53:32 +0400

    Update identifiers

--- src/everlast-office.dok32.com.js
+++ src/everlast-office.dok32.com.js
@@ -1,11 +1,9 @@
-import {addDownloadCSV} from "./addDownloadCSV.js";
+import {addFunctionButtons} from "./addFunctionButtons.js";
 //import {addPagination} from "./addPagination.js";
-import {addSendSMS} from "./addSendSMS.js";
 
 function everlastOfficeDok32Com(){ // This function scans the page periodically to make changes.
     if (!document.querySelector("#download-CSV-button")) {
-		addDownloadCSV();
-		addSendSMS();
+		addFunctionButtons();
     }
 	if (document.querySelector("[ng-click='getReport()']")){
 	    //addPagination();
