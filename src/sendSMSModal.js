1bd55dcb0aa1362118a2da081039be78bde5b426
Menas Erian <meena5.erian@gmail.com>
2020-10-05 18:13:22 +0400

    Update sendSMSModal.js

--- src/sendSMSModal.js
+++ src/sendSMSModal.js
@@ -10,14 +10,17 @@ function sendSMSModal(){
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
+        <h4 class="modal-title">Sending SMS to all selected <b id='selected-contacts'>...</b> contacts</h4>
       </div>
       <div class="modal-body">
-        <p>Select SMS template.</p>
-        <select id='sms-templates-list'>
-        </select>
+        <div class="form-group">
+          <label for="sms-templates-list" class="col-form-label">SMS template:</label>
+          <select class='form-control' id='sms-templates-list'>
+          </select>
+        </div>
       </div>
       <div class="modal-footer">
+        <button type="button" class="btn btn-success">Send SMS</button>
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
       </div>
     </div>
