baba8d4e4e2802cd978666d4bb84906b3562b3a7
Menas Erian <meena5.erian@gmail.com>
2020-10-08 10:13:55 +0400

    connect modal

--- src/viewReportButton.js
+++ src/viewReportButton.js
@@ -1,3 +1,4 @@
+import {reportModal} from "./reportModal.js";
 
 function runReport(){
     console.log("Running report");
@@ -6,12 +7,13 @@ function runReport(){
 window.runReport = runReport;
 
 function viewReportButton(){
-    //document.body.append(sendSMSModal());
+    document.body.append(reportModal());
     console.log("Dok32 Fixes extension: Adding new report function button \"View Report\"");
     let exportButton = document.createElement("button");
     exportButton.className = "btn btn-success btn-sm";
     exportButton.id = "view-report-button";
-    exportButton.setAttribute("onclick", `$("#reportModal").modal(); window.runReport();`);
+    exportButton.setAttribute("onclick", `$("#reportModal").modal();`);
+    exportButton.onclick = runReport;
     exportButton.innerText = "View Report";
     return exportButton;
 }
