f5a2e811d4505cf0f9857a2c9df5badf00b4e93e
Menas Erian <meena5.erian@gmail.com>
2020-10-08 13:18:20 +0400

    Add function to print circular structure

---
+++ printCircularStructure.js
@@ -0,0 +1,17 @@
+// Source: https://stackoverflow.com/questions/11616630/how-can-i-print-a-circular-structure-in-a-json-like-format#answer-11616993
+
+function printCircularStructure(circ){
+    // Note: cache should not be re-used by repeated calls to JSON.stringify.
+    var cache = [];
+    return JSON.stringify(circ, (key, value) => {
+    if (typeof value === 'object' && value !== null) {
+        // Duplicate reference found, discard key
+        if (cache.includes(value)) return;
+        // Store value in our collection
+        cache.push(value);
+    }
+    return value;
+    }, 2);
+}
+
+export {printCircularStructure};
