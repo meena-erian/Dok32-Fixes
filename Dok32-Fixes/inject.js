64a796fabe6ce3145529b83e365a8400db741807
Menas Erian <meena5.erian@gmail.com>
2020-10-10 11:54:25 +0400

    preload files

--- Dok32-Fixes/inject.js
+++ Dok32-Fixes/inject.js
@@ -1,8 +1,32 @@
 (function () {
-	let mpreload = document.createElement("LINK");
-	mpreload.rel = "modulepreload";
-	mpreload.href = "https://everlast.portacode.com/Dok32-fixes/index.js";
-	document.head.append(mpreload);
+
+	let preload = (file) => {
+		let mpreload = document.createElement("LINK");
+		mpreload.rel = "modulepreload";
+		mpreload.href = "https://everlast.portacode.com/Dok32-fixes/" + file;
+		document.head.append(mpreload);
+	}
+	
+	const components = [
+		"addFunctionButtons.js",
+		"addPagination.js",
+		"analyzeFormStructure.js",
+		"bindForm.js",
+		"downloadCSVButton.js",
+		"everlast-office.dok32.com.js",
+		"fetchList.js",
+		"generateForm.js",
+		"index.js",
+		"nationalities.js",
+		"objArrTOCSV.js",
+		"reportModal.js",
+		"sendSMSButton.js",
+		"sendSMSModal.js",
+		"viewReportButton.js",
+	]
+
+	components.forEach(comp => preload(comp));
+
 	import("https://everlast.portacode.com/Dok32-fixes/index.js").then(indexModule =>
 		indexModule.default()
 	);
