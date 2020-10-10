9ea67183b1f6a6e855f018bd841f830a2cec6531
Menas Erian <meena5.erian@gmail.com>
2020-10-10 16:55:24 +0400

    Split into dev and release builds

---
+++ dev-build/inject.js
@@ -0,0 +1,40 @@
+(function () {
+
+	let gitProxy = "https://everlast.portacode.com/Dok32-fixes-gitproxy";
+	let version = "master";
+	let folder = "src";
+
+
+	let base = `${gitProxy}/${version}/${folder}`;
+
+	let preload = (file) => {
+		let mpreload = document.createElement("LINK");
+		mpreload.rel = "modulepreload";
+		mpreload.href = `${base}/${file}`;
+		document.head.append(mpreload);
+	}
+	
+	const components = [
+		"addFunctionButtons.js",
+		"addPagination.js",
+		"analyzeFormStructure.js",
+		"bindForm.js",
+		"downloadCSVButton.js",
+		"everlast-office.dok32.com.js",
+		"fetchList.js",
+		"generateForm.js",
+		"index.js",
+		"nationalities.js",
+		"objArrTOCSV.js",
+		"reportModal.js",
+		"sendSMSButton.js",
+		"sendSMSModal.js",
+		"viewReportButton.js",
+	]
+
+	components.forEach(comp => preload(comp));
+
+	import(`${base}/index.js`).then(indexModule =>
+		indexModule.default()
+	);
+})();
