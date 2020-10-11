ac539d7f0b150f9a199955d328e68cad53a4d80a
Menas Erian <meena5.erian@gmail.com>
2020-10-11 15:59:31 +0400

    Create findInAngularApp.js

---
+++ src/findInAngularApp.js
@@ -0,0 +1,21 @@
+let $scope = window.angular.element('[ng-controller=AppController]').scope();
+function _findInAngularApp(scope, key){
+  if(scope[key]){
+    return scope[key];
+  }
+  if(scope.$$childTail){
+    let ret = findInAngularApp(scope.$$childTail, key);
+    if(ret !== undefined) return ret;
+  }
+  if(scope.$$prevSibling){
+    let ret = findInAngularApp(scope.$$prevSibling, key);
+    if(ret !== undefined) return ret;
+  }
+}
+//params = _findInAngularApp($scope, "searchParams");
+
+function findInAngularApp(key){
+  return _findInAngularApp($scope, key);
+}
+
+export {findInAngularApp};
