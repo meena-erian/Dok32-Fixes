bea8b0e1eb4095216eb544c745cbf791b37df826
Menas Erian <meena5.erian@gmail.com>
2020-10-12 14:51:52 +0400

    Update fetchList.js

--- src/fetchList.js
+++ src/fetchList.js
@@ -28,7 +28,9 @@ async function fetchList(endpoint, params, reccursion = false, limit = 100, prog
             return false;
         }
         if(typeof mergeFunc === "function"){
-            await Promise.all(response.data.list.map(mergeFunc));
+            let results = await response.data.list.map(mergeFunc);
+            console.log(results);
+            list = list.concat(results);
         }
         else{
             list = list.concat(response.data.list);
