5f612e5fd4b177f60c3cad042c137687edb4bd37
Menas Erian <meena5.erian@gmail.com>
2020-10-19 11:57:02 +0400

    Update app.dok32.com.js

--- src/app.dok32.com.js
+++ src/app.dok32.com.js
@@ -1,7 +1,8 @@
 import {trafficSpoofer} from "./trafficSpoofer.js";
 
 function appDok32Com(){
-    trafficSpoofer((json, xhr) => {console.log(`XHR: ${xhr.status} : ${xhr.responseURL} `, "\n", JSON.parse(json), "\n\n\nResponse: \n", JSON.parse(xhr.responseText));});
+    console.log("Dok32 Fixes extension loaded.");
+    trafficSpoofer(function (json, xhr) {console.log(`XHR: ${xhr.status} : ${xhr.responseURL} `, "\n", JSON.parse(json), "\n\n\nResponse: \n", JSON.parse(xhr.responseText));});
 }
 
 export {appDok32Com};
