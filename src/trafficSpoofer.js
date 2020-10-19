180d30f8282b67034264f6a090a230ae7e4c79bc
Menas Erian <meena5.erian@gmail.com>
2020-10-19 13:20:05 +0400

    Update trafficSpoofer.js

--- src/trafficSpoofer.js
+++ src/trafficSpoofer.js
@@ -5,8 +5,8 @@
  *  takes two parameters. 1- {string} body: The content being sent. 2- {XMLHttpRequest} this
  */
 function trafficSpoofer(func){
-    XMLHttpRequest.prototype.oldSend = XMLHttpRequest.prototype.send;
-    XMLHttpRequest.prototype.send = function (body) {
+    window.XMLHttpRequest.prototype.oldSend = window.XMLHttpRequest.prototype.send;
+    window.XMLHttpRequest.prototype.send = function (body) {
         func(body, this);
         console.log("Test:", body,"This:", this);
         return this.oldSend(body);
