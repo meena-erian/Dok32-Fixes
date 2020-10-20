e725197493fc9f50dab8b3e3a2c971f27030e940
Menas Erian <meena5.erian@gmail.com>
2020-10-20 10:44:18 +0400

    Connect to server

--- src/app.dok32.com.js
+++ src/app.dok32.com.js
@@ -26,7 +26,22 @@ function appDok32Com(){
             if(xhr.responseURL === "https://everlast-office.dok32.com:4443/api/clinic/appointment/item.json"){
                 if(response && request){
                     if(response.statusCode === 2){
+                        var endpoint = "https://everlast.portacode.com/appointment";
+                        var secret = "xJ4gSdyqo2*2sah";
                         console.log("Appointment udated: ", request);
+                        fetch(endpoint, {
+                            "headers": {
+                                "accept": "application/json",
+                                "authorization": "Bearer 3a4b50a1-4b8b-4d42-8494-837589ab66a8",
+                                "content-type": "application/json;charset=UTF-8",
+                            },
+                            "referrer": "https://app.dok32.com/",
+                            //"referrerPolicy": "strict-origin-when-cross-origin",
+                            "body" : JSON.stringify(request),
+                            "method" : "POST",
+                            "mode": "cors",
+                            "credentials": "include"
+                        });
                     }
                     else{
                         console.log(response);
