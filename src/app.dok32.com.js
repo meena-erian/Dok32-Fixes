353402c4c78180e4cbc664db6312a4f35993e686
Menas Erian <meena5.erian@gmail.com>
2020-10-20 17:28:05 +0400

    Retrive Creator info before connecting to the server

--- src/app.dok32.com.js
+++ src/app.dok32.com.js
@@ -1,60 +1,67 @@
-import {trafficSpoofer} from "./trafficSpoofer.js";
+import { trafficSpoofer } from "./trafficSpoofer.js";
 
-async function digestMessage(message) {
-  const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
-  const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
-  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
-  return hashHex;
+async function getMember(id) {
+    return (await fetch(`https://everlast-office.dok32.com:4443/webApi/member/item.json?id=${id}`));
 }
 
-function appDok32Com(){
+async function digestMessage(message) {
+    const msgUint8 = new TextEncoder().encode(message);                           // encode as (utf-8) Uint8Array
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);           // hash the message
+    const hashArray = Array.from(new Uint8Array(hashBuffer));                     // convert buffer to byte array
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert bytes to hex string
+    return hashHex;
+}
+
+function appDok32Com() {
     console.log("Dok32 Fixes extension loaded.");
     trafficSpoofer(
         function (json, xhr) {
-            if(!xhr || !xhr.status){
+            if (!xhr || !xhr.status) {
                 //console.log("XHR object not provided! \nXHR:", xhr, "REQUEST:", json);
                 return;
             }
             //console.log(`XHR: ${xhr.status} : ${xhr.responseURL} `);
             var request = undefined;
-            var response = undefined; 
+            var response = undefined;
             try {
                 request = JSON.parse(json);
                 //console.log("\nRequest: \n", request);
-            } catch(e) {
+            } catch (e) {
                 //console.log("Failed to parse JSON Request");
             }
             try {
                 response = JSON.parse(xhr.responseText);
                 //console.log("\nResponse: \n", response);
-            } catch(e) {
+            } catch (e) {
                 //console.log("Failed to parse JSON Request");
             }
-            if(xhr.responseURL === "https://everlast-office.dok32.com:4443/api/clinic/appointment/item.json"){
-                if(response && request){
-                    if(response.statusCode === 2){
-                        var endpoint = "https://everlast.portacode.com/appointment";
-                        var secret = "xJ4gSdyqo2*2sah";
-                        var body = JSON.stringify(response);
-                        digestMessage(`${body}-${secret}`).then(hash => {
-                            fetch(endpoint, {
-                                "headers": {
-                                    "Accept": "application/json",
-                                    "Authorization": `token ${hash}`,
-                                    "Content-Type": "application/json;charset=UTF-8",
-                                },
-                                "referrer": "https://app.dok32.com/",
-                                "referrerPolicy": "strict-origin-when-cross-origin",
-                                "body" : body,
-                                "method" : "POST",
-                                "mode": "cors",
-                                //"credentials": "include"
+            if (xhr.responseURL === "https://everlast-office.dok32.com:4443/api/clinic/appointment/item.json") {
+                if (response && request) {
+                    if (response.statusCode === 2) {
+                        getMember(response.data.creator.id).then(creator => {
+                            response.data.creator = creator;
+                            var endpoint = "https://everlast.portacode.com/appointment";
+                            var secret = "xJ4gSdyqo2*2sah";
+                            var body = JSON.stringify(response);
+                            digestMessage(`${body}-${secret}`).then(hash => {
+                                fetch(endpoint, {
+                                    "headers": {
+                                        "Accept": "application/json",
+                                        "Authorization": `token ${hash}`,
+                                        "Content-Type": "application/json;charset=UTF-8",
+                                    },
+                                    "referrer": "https://app.dok32.com/",
+                                    "referrerPolicy": "strict-origin-when-cross-origin",
+                                    "body": body,
+                                    "method": "POST",
+                                    "mode": "cors",
+                                    //"credentials": "include"
+                                });
+                                console.log("Appointment udated: ", request);
                             });
-                            console.log("Appointment udated: ", request);
                         });
                     }
-                    else{
+                    else {
                         console.log(response);
                     }
                 }
@@ -69,4 +76,4 @@ function appDok32Com(){
     );
 }
 
-export {appDok32Com};
+export { appDok32Com };
