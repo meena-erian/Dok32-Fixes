1d41377095df37f060898ad5efb0f8ac8464b685
Menas Erian <meena5.erian@gmail.com>
2021-01-26 13:20:49 +0400

    remove old files

--- Dok32-Fixes/background.js
+++
@@ -1,7 +0,0 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	console.log("Chrome Extension: tab: ", tab);
-	//chrome.tabs.executeScript(tab.ib, {
-	//	file: 'inject.js'
-	//});
-});
