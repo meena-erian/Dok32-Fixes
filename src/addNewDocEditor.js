affce6fe5038d5e311fab0c47e15aed509490d2b
Menas Erian <meena5.erian@gmail.com>
2021-01-27 17:46:18 +0400

    Ctrl+Shift+F

--- src/addNewDocEditor.js
+++ src/addNewDocEditor.js
@@ -1,10 +1,10 @@
-import {fetchList} from "./fetchList.js";
+import { fetchList } from "./fetchList.js";
 
-async function addNewDocEditor(oldEditorDiv){
+async function addNewDocEditor(oldEditorDiv) {
     var shortCodes = await fetchList('consent-form/template/keyword/choose.json');
     var oldEditorObj = window.tinymce.editors[0].selection.editor;
     console.log("oldEditorObj:", oldEditorObj);
-    var currentContent =  oldEditorObj.getContent();
+    var currentContent = oldEditorObj.getContent();
     oldEditorDiv.style.display = "none";
     var newEditorDiv = document.createElement("div");
     newEditorDiv.setAttribute("id", "new-mce-editor");
@@ -18,35 +18,35 @@ async function addNewDocEditor(oldEditorDiv){
             var newContent = editor.getContent();
             oldEditorObj.setContent(newContent);
         },
-        setup: function(editor) {
-            editor.on('change', function(e) {
+        setup: function (editor) {
+            editor.on('change', function (e) {
                 var editor = e.target;
                 var newContent = editor.getContent();
                 oldEditorObj.setContent(newContent);
             });
-            editor.on('keypress', function(e) {
+            editor.on('keypress', function (e) {
                 var editor = e.target;
-                var newContent =  editor.innerHTML;
+                var newContent = editor.innerHTML;
                 oldEditorObj.setContent(newContent);
             });
         }
     }))[0];
     window.editorSyncid = window.setInterval(() => {
-        if(oldEditorObj && newEditorObj && oldEditorObj.getContent && newEditorObj.getContent){
+        if (oldEditorObj && newEditorObj && oldEditorObj.getContent && newEditorObj.getContent) {
             var newContent = newEditorObj.getContent();
             var oldContent = oldEditorObj.getContent();
-            if(newContent !== oldContent){
+            if (newContent !== oldContent) {
                 oldEditorObj.setContent(newContent);
             }
         }
-        else{
+        else {
             window.clearInterval(window.editorSyncid);
         }
     }, 1000);
     console.log("newEditorObj:", newEditorObj);
-    if(currentContent.length){
+    if (currentContent.length) {
         newEditorObj.setContent(currentContent);
     }
 }
 
-export {addNewDocEditor};
+export { addNewDocEditor };
